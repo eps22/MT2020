55339651c8c6ad9aefe42889d0baa78b32ed6fe7
eps22 <45167070+eps22@users.noreply.github.com>
2020-01-29 19:43:52 +0100

    Update plotmedicanestrack.sh

--- Code/plotmedicanestrack.sh
+++ Code/plotmedicanestrack.sh
@@ -6,6 +6,11 @@ if [ $# -eq 4 ]
     R < PostProcessing/RUNPOST.R $1 $2 $3 $4 --no-save
 fi
 
+if [ $# -eq 7 ]
+  then
+    R < PostProcessing/RUNPOST.R $1 $2 $3 $4 $5 $6 $7 --no-save
+fi
+
 if [ $# -eq 8 ]
   then
     R < PostProcessing/RUNPOST.R $1 $2 $3 $4 $5 $6 $7 $8 --no-save
